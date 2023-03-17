096108bdfb74f6102c31bff2a38c4e8ed3f900d1
freddking <chatter.opals_0h@icloud.com>
2023-03-17 12:04:25 -0400

    Add ability to submit form and send email

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,7 +1,59 @@
-import React from 'react';
+import emailjs from "@emailjs/browser";
+import React, { useRef, useState } from "react";
 import "../styles/Contact.css";
 
 function Contact() {
+  const formReference = useRef(null);
+
+  const [form, setForm] = useState({
+    name: '',
+    email: '',
+    message: '',
+  });
+  const [loading, setLoading] = useState(false);
+
+  const handleTextFieldChange = (e) => {
+    console.log(form);
+    const { name, value } = e.currentTarget;
+
+    // Unpack current values of form fields,
+    // then change value of name]
+    setForm({...form, [name]: value})
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    emailjs.send(
+      'service_nozs4mq',  // service
+      'template_ymrbbjt', // template 
+      {
+        from_name: form.name,
+        to_name: '',
+        from_email: form.email,
+        to_email: '',
+        message: form.message
+      },
+      ''  // public key
+    )
+    .then(() => {
+      setLoading(false);
+      alert('Thank you for your time. I will get back to you as soon as possible.');
+
+      setForm({
+        name: '',
+        email: '',
+        message: ''
+      });
+    }, (error) => {
+      setLoading(false);
+      console.log(error);
+
+      alert('Something went wrong.');
+    })
+  }
+
   return (
     <>
       <div id="contact-page">
@@ -16,7 +68,9 @@ function Contact() {
         </div>
 
         <div id="contact-form-container"> 
-          <form className="contact-page-form">
+          <form className="contact-page-form"
+            ref={formReference}
+          >
             <label className="contact-form-field">
               <span>
                 Name
@@ -25,6 +79,9 @@ function Contact() {
               <input className="contact-form-input"
                 type="text"
                 placeholder="John Doe"
+                name="name"
+                value={form.name}
+                onChange={handleTextFieldChange}
               />
             </label>
 
@@ -34,8 +91,11 @@ function Contact() {
               </span>
 
               <input className="contact-form-input"
-                type="text"
+                type="email"
                 placeholder="example@email.com"
+                name="email"
+                value={form.email}
+                onChange={handleTextFieldChange}
               />
             </label>
 
@@ -47,14 +107,17 @@ function Contact() {
               <textarea className="contact-form-input"
                 rows={7}
                 placeholder="Hi Aaron! 🐔"
+                name="message"
+                value={form.message}
+                onChange={handleTextFieldChange}
               />
             </label>
 
             <button className="contact-form-submit-button"
               type="submit"
-              onClick={((e) => e.preventDefault())}
+              onClick={(e) => handleSubmit(e)}
             >
-              Submit
+              {loading ? 'Sending...' : 'Send'}
             </button>
           </form>
         </div>
