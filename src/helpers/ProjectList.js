ff337990f896082e96a36b939bd4c4ad19f6a85a
aaronbencopeland <aaronbencopeland@gmail.com>
2023-03-18 12:04:55 -0400

    added a project list file for convenience

---
+++ src/helpers/ProjectList.js
@@ -0,0 +1,39 @@
+import Project1 from "../assets/project1.jpg";
+import Project2 from "../assets/project2.jpg";
+import Project3 from "../assets/project3.jpg";
+import Project4 from "../assets/project4.jpg";
+import Project5 from "../assets/project5.jpg";
+import Project6 from "../assets/project6.png";
+
+export const ProjectList = [
+  {
+    name: "Name of Project 1",
+    image: Project1,
+    skills: "HTML, CSS, and JavaScript",
+  },
+  {
+    name: "Name of Project 2",
+    image: Project2,
+    skills: "React, Node.js, Express, and MongoDB",
+  },
+  {
+    name: "Name of Project 3",
+    image: Project3,
+    skills: "Angular, Node.js, Express, and MongoDB",
+  },
+  {
+    name: "Name of Project 4",
+    image: Project4,
+    skills: "Vue, Spring Boot, and Oracle",
+  },
+  {
+    name: "Name of Project 5",
+    image: Project5,
+    skills: "HTML, CSS, JavaScript, Servlet, JSP, and Core Java",
+  },
+  {
+    name: "Name of Project 6",
+    image: Project6,
+    skills: "Java Swing",
+  },
+];
