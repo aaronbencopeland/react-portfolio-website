cfff6ce2aaa143ed07816bfedbb3ae3f5da524f8
aaronbencopeland <aaronbencopeland@gmail.com>
2023-03-23 13:02:43 -0400

    made projects in projects page clickable, however, had to comment out Experience Route and GitHubIcon because those aspects are not yet ready to be used

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 import NavigationBar from './components/NavigationBar';
 import About from './pages/About';
 import Contact from "./pages/Contact";
+import ProjectDisplay from "./pages/ProjectDisplay";
 import Projects from "./pages/Projects";
 import Resume from "./pages/Resume";
 import Skills from "./pages/Skills";
@@ -17,6 +18,8 @@ function App() {
         <Routes>
           <Route path="/" element={<About />} />
           <Route path="/projects" element={<Projects />} />
+          {/* <Route path="/experience" element={<Experience />} /> */}
+          <Route path="project/:id" element={<ProjectDisplay />} />
           <Route path="/skills" element={<Skills />} />
           <Route path="/resume" element={<Resume />} />
           <Route path="/contact" element={<Contact />} />
